caed52e16fbc1d68504fc7c894132f75b16e5a43
Mauricio Arantes <noob-crispim@hotmail.com>
2022-04-19 00:58:16 -0300

    fix: better type class

--- src/helpers/outputFile.ts
+++ src/helpers/outputFile.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export class OutputFile {
-  public pathFolder: string;
+  private readonly pathFolder: string;
 
   constructor(outputPath?: string) {
     if (outputPath) {
