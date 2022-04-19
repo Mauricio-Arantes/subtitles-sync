2bf36536b66feccca7d1125235f06e1b0b63f55d
Mauricio Arantes <noob-crispim@hotmail.com>
2022-04-19 00:59:00 -0300

    chore: add validate in dateChange

--- src/methods/dateChange.ts
+++ src/methods/dateChange.ts
@@ -4,6 +4,15 @@ import {IDateChange} from "../interfaces";
 
 export class DateTransformer {
     public static timeChange(captions: [IDateChange], ms: number): [IDateChange] {
-        return subsrt.resync(captions, {offset: ms});
+        try {
+            if (Math.sign(captions[0].start + ms) === -1) {
+                throw new Error('Its not possible to change time before the start of the video');
+            }
+            return subsrt.resync(captions, {offset: ms});
+        } catch (error) {
+            let message = 'Error changing date';
+            if (error instanceof Error) message = error.message;
+            throw new Error(message);
+        }   
     }
 }
